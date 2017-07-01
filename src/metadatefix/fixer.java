9367b3b77b1a98a02840f3203e9d5ace14252894
Louis Heath <lh16421@bristol.ac.uk>
2017-07-01 23:13:27 +0100

    interpreted date inputs

--- src/metadatefix/fixer.java
+++ src/metadatefix/fixer.java
@@ -5,27 +5,42 @@
  */
 package metadatefix;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author louis
+ * @author Louis Heath
  */
 public class fixer {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     public static void main (String[] args) {
-      File[] files = new File("").listFiles();
-
+      File[] files = new File("files").listFiles();
+      List<String> dates = new ArrayList();
+      
       for (File file : files) {
         if (file.isFile()) {
-          System.out.println(file.getName());
-          //names.add(files.getName());
+          String name = file.getName();
+          System.out.println(name +" found");
+          String date = getDate(name);
+          System.out.println(date + " interpreted");
+          dates.add(date);
         }
       }
+    }
+    
+    public static String getDate (String name) {
+        String date = "";
+
+        String year = name.substring(4, 8);
+        String month = name.substring(8, 10);
+        String day = name.substring(10, 12);
+        
+        date = year + "." + month + "." + day;
+      
+        return date;
     }
 }
