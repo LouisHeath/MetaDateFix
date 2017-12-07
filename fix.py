c2b30ac5374313b496da4d0d8ae70867213caf15
Louis Heath <lh16421@bristol.ac.uk>
2017-12-07 19:58:38 +0000

    reinit
    restarting with python. planning to use terminal command which changes metadata

---
+++ fix.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import *
+import os
+
+#---------------------------------------------------------
+#
+# This terminal command on Mac works
+#     SetFile -d "06/16/2015 12:00:00" /Users/louis/metadata-fix/files/IMG-20150616-WA0003.jpg
+#
+#----------------------------------------------------------
